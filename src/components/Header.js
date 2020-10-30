97ef49acbcb5243ed9595024fcd6da00836677b8
nulltu <rusbent.matta@gmail.com>
2020-10-29 23:58:59 -0300

    sobre mí

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'materialize-css';
-import { Navbar, Icon, NavItem} from 'react-materialize';
+import { Navbar, Icon, NavItem } from 'react-materialize';
+import {animateScroll as scroll} from 'react-scroll'
 
 function Header() {
 
-  return (
-    <>
-      <Navbar
-        alignLinks="center"
-        id="mobile-nav"
-        menuIcon={<Icon>menu</Icon>}
-        options={{
-          draggable: true,
-          edge: 'left',
-          inDuration: 250,
-          onCloseEnd: null,
-          onCloseStart: null,
-          onOpenEnd: null,
-          onOpenStart: null,
-          outDuration: 200,
-          preventScrolling: true
-        }}
-      > 
-        <NavItem href="">Inicio</NavItem>
-        <NavItem href="">Sobre Mí</NavItem>
-        <NavItem href="">Skills</NavItem>
-        <NavItem href="">PortFolio</NavItem>
-        <NavItem href="">Contacto</NavItem>
-      </Navbar>
-    </>
-  )
-}
+    return (
+      <>
+        <Navbar
+          alignLinks="center"
+          id="mobile-nav"
+          menuIcon={<Icon>menu</Icon>}
+          options={{
+            draggable: true,
+            edge: 'left',
+            inDuration: 250,
+            onCloseEnd: null,
+            onCloseStart: null,
+            onOpenEnd: null,
+            onOpenStart: null,
+            outDuration: 200,
+            preventScrolling: true
+          }}
+        >
+          <NavItem href="">Inicio</NavItem>
+          <NavItem onClick={() => scroll.scrollTo(1000)}>Sobre Mí</NavItem>
+          <NavItem href="">Skills</NavItem>
+          <NavItem href="">PortFolio</NavItem>
+          <NavItem href="">Contacto</NavItem>
+        </Navbar>
+      </>
+    )
+  }
 
 
-export default Header
+
+  export default Header
